e8bb9cc3202394846df922440910dfae49bd852d
lord8266 <lordvishwa123@gmail.com>
2018-10-03 21:07:12 +0530

    sdl chainreaction

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef DONE
+#define DONE 1
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <map>
+#include <cstdio>
+using std::pair;
+using std::set;
+using std::map;
+using std::string;
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
+using std::vector;
+using std::begin;
+using std::end;
+
+#endif
