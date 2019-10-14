b5ba0fdd00b5f1d1d3c0947fdddf6dd6b1b7be56
lord8266 <lordvishwa123@gmail.com>
2019-10-14 11:56:25 +0530

    update_a0

--- sources/Makefile.m
+++
@@ -1,9 +0,0 @@
-P=$(m)
-pks=
-CFLAGS=-Wall -Werror -Wno-unused-variable  -std=gnu++17 -g
-
-OBJECTS=
-LDLIBS=
-
-all:
-	g++ $(CFLAGS) $(P).cc $(OBJECTS) -o $(P) $(LDLIBS)
--- sources/check2.lua
+++
@@ -1,9 +0,0 @@
-file1 =io.input("display_manager.cc")
-file2 =io.output("test.cc")
-
-a=file1:read("a")
-
-b,n=a:gsub("%&boxes(%[%a+[+-]*%a*%])->(%a+)","boxes%1.%2")
-print(n)
-file2:write(b)
-file2:close()
